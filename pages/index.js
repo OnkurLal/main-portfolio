6d4e4ee45d95b385d6c2f8552ee20b99e5de2548
Onkur Lal <onkur24lal@gmail.com>
2022-12-30 04:06:20 -0500

    added contact form

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import Head from "next/head";
 import Button from "../components/Button";
 import Link from "next/link";
 import Cursor from "../components/Cursor";
+import ContactForm from "../components/ContactForm";
 
 // Local Data
 import data from "../data/portfolio.json";
@@ -145,13 +146,7 @@ export default function Home() {
           <div>
             <h1 className="text-2xl text-bold">Contact</h1>
             <div className="mt-10">
-              <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-                LET&apos;S WORK
-              </h1>
-              <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-                TOGETHER
-              </h1>
-              <Button type="primary">Schedule a call</Button>
+              <ContactForm />
             </div>
           </div>
         </div>
