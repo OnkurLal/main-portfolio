2ee07b0d2fa8191c324300e81231367c2de88174
Onkur Lal <onkur24lal@gmail.com>
2023-11-27 09:56:51 -0500

    added skills

--- components/SkillsMap/index.js
+++ components/SkillsMap/index.js
@@ -4,7 +4,7 @@ export default function SkillsMap({ skill }) {
   return (
     <div key={skill.id} className="flex justify-center items-center">
       <h1>{skill.name}</h1>
-      <img src={skill.imgSrc} alt={skill.alt} />
+      <img src={skill.imgSrc} alt={skill.alt} width={48} />
     </div>
   );
 }
